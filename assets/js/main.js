078a6f538b5ec69d7fffad447ee24f9f6bc72f21
Hung Do <hung.mh.do@gmail.com>
2022-03-18 12:47:31 -0400

    add header shadow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,25 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+    const header = document.querySelector('header');
+    // When the scroll is greater than 50vp, add the scroll-header class to the header tag
+    if (window.scrollY > 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
 
-
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
-
-/* Link active work */ 
-
+/* Link active work */
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== LIGHT DARK THEME ===============*/ 
-
+/*=============== LIGHT DARK THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
