957e1e3902226bd91f9ea4b9ada89adfbc31c506
Hung Do <hung.mh.do@gmail.com>
2022-03-18 16:53:05 -0400

    styling work + add mixitup animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,28 @@ modalClose.forEach((mc) => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card',
+    },
+    animation: {
+        duration: 300,
+    },
+});
 
 /* Link active work */
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork() {
+    linkWork.forEach((lw) => {
+        lw.classList.remove('active-work');
+    });
+    this.classList.add('active-work');
+}
+
+linkWork.forEach((lw) => {
+    lw.addEventListener('click', activeWork);
+});
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
