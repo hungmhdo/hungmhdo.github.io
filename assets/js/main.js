25bdc84bd05d4d7b3ac7b3a7b09243b40d5d51f5
Hung Do <hung.mh.do@gmail.com>
2022-03-18 20:36:50 -0400

    scrollbar + scroll navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,6 +79,27 @@ let swiperAwards = new Swiper('.awards__container', {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.add('active-link');
+        } else {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
 
 /*=============== LIGHT DARK THEME ===============*/
 
